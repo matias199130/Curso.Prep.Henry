27a125a9f46838bee1586fb2ca587d6f0a26b698
matias199130 <matiasromero20121991@gmail.com>
2021-12-26 08:47:23 -0300

    prueba

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,6 +8,7 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  "prueba de commiteo"
 }
 
 function agregarMetodoPrototype(Constructor) {
